0a64ac5a9eabd663af1b8ee58ac5b4607fad8b63
Jacky Yang <jackyyang1670@gmail.com>
2024-11-17 19:08:57 -0800

    Updated files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+import Nav from "./components/Nav";
+function App() {
+  // We will use the Route component, path attribute, and element attribute to specify each route.
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/currencies" element={<Currencies/>}/>
+        <Route path="/price/:symbol" element={<Price/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App
